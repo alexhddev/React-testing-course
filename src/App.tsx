a2e07257eb855f6619ccd950b16d28d2f1f1001b
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-03-07 09:27:54 +0200

    Testing for errors and text matchers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import { PostWithComment } from "./components/3UserInteraction/PostWithComments";
+import { ShoppingList1 } from "./components/4Errors/ShoppingList1";
+import { ShoppingList2 } from "./components/4Errors/ShoppingList2";
 
 function App() {
-  
+
+  const ingredients = ['Apples', 'Bananas', 'Ham', 'Bread', 'Bread']
+
+  const someFunction = (selectedItem: string) => { 
+    console.log(`Selected ${selectedItem}`)
+  }
 
   return (
     <>
-     <PostWithComment
-      content="The sky is blue"
-      user="Alex"
-     />
+      <h2>Shopping list 1:</h2>
+      {/* <ShoppingList1
+        groceries={ingredients}
+        selectItem={someFunction}
+      /> */}
+      <h2>Shopping list 2:</h2>
+      <ShoppingList2
+        groceries={ingredients}
+        selectItem={someFunction}
+      />
     </>
   )
 }
